2a3f63321debe916fe7bf99ed5a8092f0179a08a
Alfred <kalumunabisimwa5@gmail.com>
2022-05-06 12:04:15 +0200

    fix linters

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from 'react';
 
 const BookList = () => {
   const books = [
     {
       id: 1,
-      title: "Linters",
-      author: "alfred",
+      title: 'Linters',
+      author: 'alfred',
     },
     {
       id: 2,
-      title: "Jsx",
-      author: "Audry",
+      title: 'Jsx',
+      author: 'Audry',
     },
   ];
   return (
     <ul>
       {books.map((book) => (
         <li key={book.id}>
-          {book.title} {book.author}
+          {book.title}
+          {' '}
+          {book.author}
           <button type="button">Remove</button>
         </li>
       ))}
