25afd76f67117d60eb6279360e729218c6aeb1e7
Alfred <kalumunabisimwa5@gmail.com>
2022-05-13 08:27:33 +0200

    fix

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CategoryFilter = ({ onChangeFilter, filter }) => {
+  const categories = [
+    'All',
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
+
+  return (
+    <div className="filter-book d-flex flex-center">
+      <select
+        name="filter"
+        value={filter}
+        id="filter"
+        required
+        onChange={onChangeFilter}
+      >
+        {categories.map((categ) => (
+          <option value={categ} key={categ}>
+            {categ}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+CategoryFilter.defaultProps = {
+  onChangeFilter: () => {},
+  filter: '',
+};
+
+CategoryFilter.propTypes = {
+  onChangeFilter: PropTypes.func,
+  filter: PropTypes.string,
+};
+
+export default CategoryFilter;
